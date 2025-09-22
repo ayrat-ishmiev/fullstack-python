04c55c5af03e44d1126d1f2d3b4ff931ced5c97a
ayrat-ishmiev <ayratishmiev@gmail.com>
2025-09-22 17:37:25 +0500

    Добавить строку песни

--- ishmiev_ayrat.py
+++ ishmiev_ayrat.py
@@ -2,6 +2,7 @@ def main():
     print("Ишмиев Айрат")
     print("ИВТ-ИСУ-202Б, 2 подгруппа")
     print("22.09.2025")
+    print("Never gonna give you up")
 
 if __name__ == "__main__":
     main()
