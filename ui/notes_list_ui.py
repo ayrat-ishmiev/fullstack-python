25a593c4d239221620a1b4f5f1ecfd828a50334f
Adelina11 <31268282+Adelina11@users.noreply.github.com>
2025-12-08 22:17:08 +0500

    Надпись не обрезается

--- ui/notes_list_ui.py
+++ ui/notes_list_ui.py
@@ -64,6 +64,7 @@ class Ui_Form(object):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
         self.label.setText(_translate("Form", "Список конспектов"))
+        self.label.setMinimumWidth(self.label.sizeHint().width())
         self.pushButton.setText(_translate("Form", "+ Добавить конспект"))
         self.textBrowser.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
